6182d5ee5df73807696bcd2bf096677345d5b50b
NacamaTech <nachocaballeromarin@gmail.com>
2026-06-20 16:43:33 +0200

    Initial Commit

---
+++ PersonalDashboardTests/PersonalDashboardTests.swift
@@ -0,0 +1,18 @@
+//
+//  PersonalDashboardTests.swift
+//  PersonalDashboardTests
+//
+//  Created by Nachete on 20/06/2026.
+//
+
+import Testing
+
+struct PersonalDashboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
